034a00bd2ae473cead6415ca76f0d12df28bb2da
eempc <evc484@gmail.com>
2019-08-29 15:56:38 +0100

    some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-crash-todolist';
+  name: string = 'Ralph';
+  age: number = 8;
+  numberArray: number[] = [3, 4, 5];
+
+  constructor() {
+    console.log("hello");
+    this.name = 'Ted';
+    console.log(this.name);
+    this.changeName('Lisa');
+    console.log(this.name);
+  }
+
+  changeName(name:string):void {
+    this.name = name;
+  }
 }
